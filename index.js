60c30ee04e652c8de7886c7c5769717aa7e95ed9
Brooke Gill <brooke.gill25@gmail.com>
2018-11-07 18:03:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(line, name) {
   return (`Welcome, ${name}. You are number ${line.length} in line.`);
 }
 
-function nowServing(line, name) {
+function nowServing(line) {
   if (line.length === 0 ) {
     return "There is nobody waiting to be served!";
   } else {
